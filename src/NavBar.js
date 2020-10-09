efc1f7349190f59f4a838a1b8097f648a6e5502c
Stephen McBride <stevemcbride3@gmail.com>
2020-10-09 03:03:34 -0400

    Added navbar

---
+++ src/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+    return (
+        <nav class="nav_links">
+            <ul>
+                <li>
+                    <a href="/work">PHOTOGRAPHY</a>
+                </li>
+                <li>
+                    <a href="/videography">VIDEOGRAPHY</a>
+                </li>
+                <li>
+                    <a href="/content-creation">CONTENT CREATION</a>
+                </li>
+                <li>
+                    <a href="/about">ABOUT</a>
+                </li>
+                <li>
+                    <a href="/contact">CONTACT</a>
+                </li>
+            </ul>
+        </nav>
+    )
+}
