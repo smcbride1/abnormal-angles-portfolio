adc3e0f540ba653f756bafcf7ab7152f28955f50
Stephen McBride <stevemcbride3@gmail.com>
2020-11-07 08:28:49 -0500

    Added blur effect to nav bar, changed font families, added service helper

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import NavBar from './NavBar.js';
 import Video from "./media/videos/preview.mp4";
 import Logo from "./logo.svg";
+import CameraIcon from "./camera-icon-white.svg";
+import LaptopIcon from "./laptop-icon-white.svg";
+import VideoIcon from "./video-icon-white.svg";
 import IntroLogo from "./intro-logo.svg";
 import ContentPreviewContainer from './ContentPreviewContainer.js';
 
@@ -14,24 +17,27 @@ function App() {
         <NavBar/>
       </header>
       <div className="wrapper">
+        <img src={Logo} className="home-page-logo"/>
         <h1 className="aa-text">ABNORMAL ANGLES</h1>
         <br/>
         <br/>
-        <ul className="landing_page_links">
-          <li>
-          <a href="/work">📷 PHOTOGRAPHY</a>
+        <h2>What kind of services are you looking for?</h2>
+        <ul className="questionaire-questions">
+          <li className="questionaire-answer" id="questionaire-step1-photos">
+            <img src={CameraIcon} className="questionaire-icon" alt=""/>
+            <p>Photos</p>
           </li>
-          <li>
-              <a href="/videography">🎥 VIDEOGRAPHY</a>
+          <li className="questionaire-answer" id="questionaire-step1-videos">
+            <img src={VideoIcon} className="questionaire-icon" alt=""/>
+            <p>Video</p>
           </li>
-          <li>
-              <a href="/content-creation">💻 CONTENT CREATION</a>
+          <li className="questionaire-answer" id="questionaire-step1-content">
+            <img src={LaptopIcon} className="questionaire-icon" alt=""/>
+            <p>Content</p>
           </li>
         </ul>
-        <br/>
-        <br/>
-        <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</h2>
       </div>
+      <div id="main-page-reel-overlay"/>
       <video autoPlay loop muted src={Video} type="video/mp4" id="main-page-reel"/>
       <ContentPreviewContainer/>
     </div>
